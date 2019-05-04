bc5967be685f6afb6645f5e31a6ae312d8571b5f
Josh Gearheart <other.josh.gearheart@gmail.com>
2019-05-03 19:07:25 -0700

    add support for numbers to 99,999

--- lib/longifier.rb
+++ lib/longifier.rb
@@ -1,7 +1,7 @@
 class Numeral
 
-  def initialize(number)
-    @number = number
+  def initialize(num)
+    @number = num
   end
 
   def number
@@ -9,17 +9,6 @@ class Numeral
   end
 
   def to_words
-    ones_place = {
-      '1' => 'one',
-      '2' => 'two',
-      '3' => 'three',
-      '4' => 'four',
-      '5' => 'five',
-      '6' => 'six',
-      '7' => 'seven',
-      '8' => 'eight',
-      '9' => 'nine'
-    }
     tweens_and_teens = {
       '10' => 'ten',
       '11' => 'eleven',
@@ -32,6 +21,17 @@ class Numeral
       '18' => 'eighteen',
       '19' => 'nineteen',
     }
+    ones_place = {
+      '1' => 'one',
+      '2' => 'two',
+      '3' => 'three',
+      '4' => 'four',
+      '5' => 'five',
+      '6' => 'six',
+      '7' => 'seven',
+      '8' => 'eight',
+      '9' => 'nine'
+    }
     tens_place = {
       '2' => 'twenty',
       '3' => 'thirty',
@@ -42,24 +42,69 @@ class Numeral
       '8' => 'eighty',
       '9' => 'ninety',
     }
-    words = String.new()
-    numbers = @number.to_s.split('')
-    
-    numbers.each_with_index do |number_str, i|
-      if numbers.length == 2
-        if (numbers[numbers.length - 2] == '1') & (i == numbers.length - 2)
-          digits_to_fetch = number_str.concat(numbers[i + 1])
-          a_word = tweens_and_teens.fetch(digits_to_fetch)
-          words.concat(a_word)
-          break
+    hundreds_place = {
+      '1' => 'one hundred',
+      '2' => 'two hundred',
+      '3' => 'three hundred',
+      '4' => 'four hundred',
+      '5' => 'five hundred',
+      '6' => 'six hundred',
+      '7' => 'seven hundred',
+      '8' => 'eight hundred',
+      '9' => 'nine hundred',
+    }
+    thousands_place = {
+      '1' => 'one thousand',
+      '2' => 'two thousand',
+      '3' => 'three thousand',
+      '4' => 'four thousand',
+      '5' => 'five thousand',
+      '6' => 'six thousand',
+      '7' => 'seven thousand',
+      '8' => 'eight thousand',
+      '9' => 'nine thousand',
+    }
+    tween_thousands_place = {
+      '10' => 'ten thousand',
+      '11' => 'eleven thousand',
+      '12' => 'twelve thousand',
+      '13' => 'thirteen thousand',
+      '14' => 'fourteen thousand',
+      '15' => 'fifteen thousand',
+      '16' => 'sixteen thousand',
+      '17' => 'seventeen thousand',
+      '18' => 'eighteen thousand',
+      '19' => 'nineteen thousand',
+    }
+    words = Array.new
+    nums = @number.to_s.split('')
+
+
+    if (nums.length == 2) & (nums[0] == '1')
+      words.unshift(tweens_and_teens.fetch(@number.to_s))
+    else
+      nums_reversed = nums.reverse
+      nums_reversed.each_with_index do |num, index|
+        # if index == 5
+          # words.unshift()
+        if (index == 4) & (nums_reversed[4] == '1')
+          words.unshift(tween_thousands_place.fetch(num.concat(nums_reversed[index - 1])))
+        elsif (index == 4) & (nums_reversed[4] != '1')
+          words.unshift(tens_place.fetch(num))
+        elsif (index == 3) & (nums_reversed[4] == '1')
+          next
+        elsif (index == 3) & (nums_reversed[4] != '1')
+          words.unshift(thousands_place.fetch(num))
+        elsif index == 2
+          words.unshift(hundreds_place.fetch(num))
+        elsif index == 1
+          words.unshift(tens_place.fetch(num))
+        elsif index == 0
+          words.unshift(ones_place.fetch(num))
         end
       end
-      if numbers.length == 1
-        a_word = ones_place.fetch(number_str)
-        words.concat(a_word)
-      end
     end
-    words
+    words.join(' ')
   end
 
 end
--- spec/longifier_spec.rb
+++ spec/longifier_spec.rb
@@ -12,4 +12,24 @@ describe('Numeral#to_words') do
     expect(Numeral.new(17).to_words()).to(eq('seventeen'))
   end
 
+  it('returns the written-out version of a number from 20 to 99') do
+    expect(Numeral.new(27).to_words()).to(eq('twenty seven'))
+  end
+
+  it('returns the written-out version of a number from 100 to 999') do
+    expect(Numeral.new(365).to_words()).to(eq('three hundred sixty five'))
+  end
+
+  it('returns the written-out version of a number from 1000 to 9999') do
+    expect(Numeral.new(6899).to_words()).to(eq('six thousand eight hundred ninety nine'))
+  end
+
+  it('returns the written-out version of a number from 10000 to 19999') do
+    expect(Numeral.new(16897).to_words()).to(eq('sixteen thousand eight hundred ninety seven'))
+  end
+
+  it('returns the written-out version of a number from 20000 to 99999') do
+    expect(Numeral.new(76241).to_words()).to(eq('seventy six thousand two hundred forty one'))
+  end
+
 end
